641f6a23e002e6e6a77b4c4feffaa2835baf457b
Aigerim Abdurakhmanova <aigerim.abdurakhmanova@IMAC-3-14.local>
2022-06-23 18:05:45 +0600

    sign up

--- Booking/Booking/Network/APIManager.swift
+++ Booking/Booking/Network/APIManager.swift
@@ -43,31 +43,31 @@ class APIManager {
         }
     }
     
-    func logInApi(login: LoginModel, completionHandler: @escaping (Bool) -> ()) {
-        let header: HTTPHeaders = [.accept("*/*"), .contentType("application/json")]
-
-        AF.request(loginUrl, method: .post, parameters: login, encoder: JSONParameterEncoder.default, headers: header).response { response in
-            debugPrint(response.result)
-            switch response.result {
-            case.success(let data):
-               // print(data!)
-                do{
-                    let json = try JSONSerialization.jsonObject(with: data!)
-                    print(json)
-                    if response.response?.statusCode == 200 {
-                        completionHandler(true)
-                    }else {
-                        completionHandler(false)
-                    }
-                }catch{
-                    print(error)
-                }
-            case.failure(let error):
-                print(error)
-                completionHandler(false)
-            }
-            
-        }
-    }
+//    func logInApi(login: LoginModel, completionHandler: @escaping (Bool) -> ()) {
+//        let header: HTTPHeaders = [.accept("*/*"), .contentType("application/json")]
+//
+//        AF.request(loginUrl, method: .post, parameters: login, encoder: JSONParameterEncoder.default, headers: header).response { response in
+//            debugPrint(response.result)
+//            switch response.result {
+//            case.success(let data):
+//               // print(data!)
+//                do{
+//                    let json = try JSONSerialization.jsonObject(with: data!)
+//                    print(json)
+//                    if response.response?.statusCode == 200 {
+//                        completionHandler(true)
+//                    }else {
+//                        completionHandler(false)
+//                    }
+//                }catch{
+//                    print(error)
+//                }
+//            case.failure(let error):
+//                print(error)
+//                completionHandler(false)
+//            }
+//            
+//        }
+//    }
     
 }
